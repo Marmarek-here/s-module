3785a0e9f4ea27767978cb7ef062fb4ad8ab258d
Marmarek <orumets.marek@gmail.com>
2026-05-29 21:45:53 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-from .s import s
-
-class CallableModule(sys.modules[__name__].__class__):
-    def __call__(self, length=0):
-        return s(length)
-
-sys.modules[__name__].__class__ = CallableModule
